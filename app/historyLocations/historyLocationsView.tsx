aa97499ced142deb14959957d1a12c4415c05ced
Kevin Licona <kevinliconasilva@gmail.com>
2026-02-16 13:20:39 -0600

    Update historyLocationsView.tsx

--- app/historyLocations/historyLocationsView.tsx
+++ app/historyLocations/historyLocationsView.tsx
@@ -1,7 +1,7 @@
 //pantalla de puntos en el mapa
 
 import { HistoryLocationsView } from "@/components/location/historyLocationView";
-import { LocationView } from "@/components/location/locationView";
+
 
 export default function HomeScreen(){
     return(
