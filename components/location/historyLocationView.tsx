800e7083ca78abf6ae5d083f535b4fc3069e4150
Kevin Licona <kevinliconasilva@gmail.com>
2026-02-16 23:51:12 -0600

    Update historyLocationView.tsx

--- components/location/historyLocationView.tsx
+++ components/location/historyLocationView.tsx
@@ -2,10 +2,6 @@ import { useEffect, useRef, useState,} from 'react';
 import { StyleSheet,View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
-import * as Location from 'expo-location';
-import { Link} from 'expo-router';
-import { useRouter } from 'expo-router';
-
 //para la conexion con firebase
 import { collection, addDoc, getDocs } from "firebase/firestore"; 
 import { db } from '@/lib/firebase';
